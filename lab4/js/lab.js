9baa5df178d92797fe46bad8fb93fdb257d61760
gobeklitech <102619811+gobeklitech@users.noreply.github.com>
2022-04-19 10:55:25 -0700

    Did lab 4!

---
+++ lab4/js/lab.js
@@ -0,0 +1,20 @@
+// A high-level overview of how to build an image classifier.
+// This would be done more appropriately in Python.
+// Edward Hayden.
+// 4.19.22
+
+
+
+
+	// Pseudocode: 
+	//                    Obtain Dataset ,
+	// 					  Pre-process data:
+	// 					   - Split dataset into training set, test set
+	// 					   - Normalize / format data as needed
+	// 					  Feature Engineering,
+	// 					  Model Selection:
+	// 					  	-Build model from scratch or select package(s)
+	// 					  Train model on training set,
+	// 					  Evaluate model, Tune parameters
+	// 					  Test on Test Set						  	
+	// 						
