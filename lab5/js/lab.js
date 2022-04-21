a53936e36486ea1883144c74d515805c6469f2e3
gobeklitech <102619811+gobeklitech@users.noreply.github.com>
2022-04-21 13:35:55 -0700

    Added some HTML, CSS, JS

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+// Author: Edward Hayden
+// Date: 4/21/22
+
+
+// Variable Declaration:
+
+var make = "GMC";
+var model = "Safari";
+var year = 1989;
+var color = "Silver / Red";
+var age = 2022 - year;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Age: " + age + "<br>");
+
+
+
