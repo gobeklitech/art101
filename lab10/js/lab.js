07a1fed337cc3ec7b6a8f1f8c28024f3823d68a9
gobeklitech <102619811+gobeklitech@users.noreply.github.com>
2022-05-09 23:52:20 -0700

    Some events and forms

---
+++ lab10/js/lab.js
@@ -0,0 +1,27 @@
+// Teddy Hayden 
+
+// Monday, May 9th, 2022
+
+
+let outputEl = document.getElementById("output");
+
+let myButton = document.getElementById("my-button");
+
+let myUsername= document.getElementById("user-name");
+
+
+
+function sortName() {
+
+	var userName = myUsername.value;
+
+	var nameArray = userName.split("");
+
+	var sortedName = nameArray.sort().join("");
+
+	outputEl.innerHTML = sortedName;
+
+} 
+
+
+myButton.addEventListener("click",sortName);
